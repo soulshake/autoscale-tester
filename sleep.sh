6a8af624c27183030ba582e73899fa2ffe700bf0
AJ Bowen <aj@soulshake.net>
2017-08-24 12:19:35 +0200

    SHOULDERROR

--- sleep.sh
+++ sleep.sh
@@ -7,14 +7,14 @@ echo "START: $(date)"
 echo "SLEEPING: ${SLEEP} seconds"
 
 if [ "${KEEPALIVE}" ]; then
-    echo "KEEPING ALIVE"
-    while [ "${SECONDS}" -lt "${SLEEP}" ]; do
-        echo "${SECONDS} < ${SLEEP}"
-        sleep 60
-    done
+  echo "KEEPING ALIVE"
+  while [ "${SECONDS}" -lt "${SLEEP}" ]; do
+    echo "${SECONDS} < ${SLEEP}"
+    sleep 60
+  done
 else
-    echo "NOT KEEPING ALIVE"
-    sleep "${SLEEP}"
+  echo "NOT KEEPING ALIVE"
+  sleep "${SLEEP}"
 fi
 
 echo "END: $(date)"
