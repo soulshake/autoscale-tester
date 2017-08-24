4acabdda4cfe7adf09306903fb06569327c311bf
AJ Bowen <aj@soulshake.net>
2017-08-24 12:04:58 +0200

    Keep alive

--- sleep.sh
+++ sleep.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
+echo "SECONDS: ${SECONDS}"
+
+SECONDS=0
 echo "START: $(date)"
-echo "SLEEPING: ${SLEEP}"
-sleep "${SLEEP}"
+echo "SLEEPING: ${SLEEP} seconds"
+
+if [ "${KEEPALIVE}" ]; then
+    echo "KEEPING ALIVE"
+    while [ "${SECONDS}" -lt "${SLEEP}" ]; do
+        echo "${SECONDS} < ${SLEEP}"
+        sleep 60
+    done
+else
+    echo "NOT KEEPING ALIVE"
+    sleep "${SLEEP}"
+fi
+
 echo "END: $(date)"
+duration=$SECONDS
+echo "$((duration / 60)) minutes and $((duration % 60)) seconds elapsed."
