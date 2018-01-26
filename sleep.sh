bb7b2d8f7a8a089b69bf7b131fc3e20a481fce2c
AJ Bowen <aj@soulshake.net>
2018-01-25 18:02:43 -0800

    Send output to stderr instead of stdout

--- sleep.sh
+++ sleep.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
-echo "SECONDS: ${SECONDS}"
+stderr_echo() {
+    >&2 echo "$@"
+}
+
+echo=stderr_echo
+
+$echo "SECONDS: ${SECONDS}"
 
 SECONDS=0
-echo "START: $(date)"
-echo "SLEEPING: ${SLEEP} seconds"
+$echo "START: $(date)"
+$echo "SLEEPING: ${SLEEP} seconds"
 
 if [ "${KEEPALIVE}" == true ]; then
-  echo "KEEPING ALIVE"
+  $echo "KEEPING ALIVE"
   while [ "${SECONDS}" -lt "${SLEEP}" ]; do
-    echo "${SECONDS} < ${SLEEP}"
+    $echo "${SECONDS} < ${SLEEP}"
     sleep 300
   done
 else
-  echo "NOT KEEPING ALIVE"
+  $echo "NOT KEEPING ALIVE"
   sleep "${SLEEP}"
 fi
 
-echo "END: $(date)"
+$echo "END: $(date)"
 duration=$SECONDS
-echo "$((duration / 60)) minutes and $((duration % 60)) seconds elapsed."
+$echo "$((duration / 60)) minutes and $((duration % 60)) seconds elapsed."
